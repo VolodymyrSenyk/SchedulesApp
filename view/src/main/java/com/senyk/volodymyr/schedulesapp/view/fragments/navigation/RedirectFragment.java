52c005d6c85fd0af2c2bc9b23515da72342b5a0c
Volodymyr Senyk <Volodymyr_Senyk@epam.com>
2019-11-20 18:44:20 +0200

    SchedulesApp - navigation added

--- view/src/main/java/com/senyk/volodymyr/schedulesapp/view/fragments/navigation/RedirectFragment.java
+++
@@ -1,6 +0,0 @@
-package com.senyk.volodymyr.schedulesapp.view.fragments.navigation;
-
-import com.senyk.volodymyr.schedulesapp.view.fragments.base.BaseFragment;
-
-public class RedirectFragment extends BaseFragment {
-}
