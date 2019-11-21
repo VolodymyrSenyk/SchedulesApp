2a9d50dc6c22a0fd911bee04b2aecb2a52928b9a
Volodymyr Senyk <Volodymyr_Senyk@epam.com>
2019-11-21 21:19:54 +0200

    SchedulesApp - resources added

--- view/src/main/java/com/senyk/volodymyr/schedulesapp/di/component/AppComponent.java
+++ view/src/main/java/com/senyk/volodymyr/schedulesapp/di/component/AppComponent.java
@@ -3,7 +3,6 @@ package com.senyk.volodymyr.schedulesapp.di.component;
 import com.senyk.volodymyr.schedulesapp.di.module.ViewModelsModule;
 import com.senyk.volodymyr.schedulesapp.view.fragments.base.BaseFragment;
 
-import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import dagger.Component;
